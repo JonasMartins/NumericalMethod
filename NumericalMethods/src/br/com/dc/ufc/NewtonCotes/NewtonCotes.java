dbe3e5f295531820d739c113127a85292b2d32b3
JonasMartins <jonas.s.martins@hotmail.com>
2016-04-02 14:26:32 -0300

    First Structure of Calculations

---
+++ NumericalMethods/src/br/com/dc/ufc/NewtonCotes/NewtonCotes.java
@@ -0,0 +1,18 @@
+package br.com.dc.ufc.NewtonCotes;
+
+public class NewtonCotes {
+
+	/*	Trapezium rule  */
+	private double error;
+	private double a;
+	private double b;
+	private int order;
+	public NewtonCotes(int order){
+		/*	constructor for trapezium rule	*/
+		this.order = order;
+	}
+	
+	
+	
+	
+}
--- NumericalMethods/src/br/com/dc/ufc/main/Main.java
+++ NumericalMethods/src/br/com/dc/ufc/main/Main.java
@@ -1,14 +1,6 @@
 package br.com.dc.ufc.main;
 
 public class Main {
-	
-		//"Initial Commit"
-	
-		//try to commi
-		
-		
-		/*
-			ADD A REMOTE COMMIT .......
-		*/
+
 	
 }

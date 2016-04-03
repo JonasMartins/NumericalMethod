03bb0c25061f52a10f0b936d410effaf024d90ff
JonasMartins <jonas.s.martins@hotmail.com>
2016-04-03 11:02:53 -0300

    Trapezium method Commented Version

--- NumericalMethods/src/br/com/dc/ufc/NewtonCotes/NewtonCotes.java
+++ NumericalMethods/src/br/com/dc/ufc/NewtonCotes/NewtonCotes.java
@@ -2,17 +2,76 @@ package br.com.dc.ufc.NewtonCotes;
 
 public class NewtonCotes {
 
-	/*	Trapezium rule  */
 	private double error;
-	private double a;
-	private double b;
-	private int order;
-	public NewtonCotes(int order){
-		/*	constructor for trapezium rule	*/
+	private double a  = -0.3;
+	private double b = 3.7;
+	private int order, n=1;
+	double h = (b-a);
+	private double limit=0;
+	private double integralValue=0;
+	
+	
+	/*	fx = x⁵ - 2x³ + 4x +5 	*/
+	/*	f'x = 5x⁴ - 6x² + 4	*/
+	/*	f''x = 20x³ - 12x	*/
+	/*	f'''x = 60x² -12	*/
+	/*	f''''x = 120x	*/
+	/*	f'''''x = 120	*/
+	
+	public NewtonCotes(int order, double error){
+		
 		this.order = order;
+		this.error = error;
+	}
+	public double fx(double x){
+		// return Math.pow(x, 5) - 2*(Math.pow(x, 3)) + 4*x + 5; // função inicial
+		return Math.pow(x, 3) - 2*(Math.pow(x, 2)) + x; //função mais simples
 	}
 	
+	public double TrapeziumMethod(){		
+		double x,ai,bi,aux=0;
+		do {
+			ai=a;
+			//n=1;
+			do {
+			System.out.println("n: "+n);
+			System.out.println("ai before: "+ai);
+			x = (h/n)/2;
+			//limit = 20*(Math.pow(x, 3)) - 12*x; // segunda integral função inicial
+			limit = 6*x - 4; // segunda integral de função mais simples
+			limit*=(Math.pow((h/n), 3));
+			limit/=12;
+			System.out.println("bi-ai = "+h/n);
+			//System.out.println("x' = "+x);
+			bi = ai+(h/n);
+			n++;
+			System.out.println("ai after: "+ai);		
+			System.out.println("bi after: "+bi);
+			aux += (bi-ai)/2*(fx(ai)+fx(bi));
+			
+			ai = a + (n-1)*(h/n);
+			
+			System.out.println("limite: "+limit);
+			integralValue+=aux;
+			//System.out.println(integralValue);
+			System.out.println("final: "+integralValue);
+			}while(bi<b);
+			integralValue=aux=0;
+			System.out.println("Saiu!");
+		}while(Math.abs(limit) > error);
+		return integralValue;
+	}
 	
+	public void run(){
+		switch(order){
+		case 1:
+			TrapeziumMethod();
+			break;
+		default:
+			System.out.println("Bye");
+			break;
+		}
+	}
 	
 	
 }
--- NumericalMethods/src/br/com/dc/ufc/main/Main.java
+++ NumericalMethods/src/br/com/dc/ufc/main/Main.java
@@ -1,6 +1,34 @@
 package br.com.dc.ufc.main;
 
+import javax.swing.JOptionPane;
+
+import br.com.dc.ufc.NewtonCotes.NewtonCotes;
+
 public class Main {
 
-	
+	public static void main(String[] args) {
+		/*
+		int option=0;
+		double error=0;
+		
+		try {
+			option = Integer.parseInt(JOptionPane.showInputDialog(null, "enter your degree aproximation"));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		try {
+			error = Double.parseDouble(JOptionPane.showInputDialog(null, "enter your error limitation"));
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+		NewtonCotes closedMethods = new NewtonCotes(option,error);	
+			
+			*/
+		
+		NewtonCotes closedMethods = new NewtonCotes(1,1);
+		
+		closedMethods.run();
+		
+	}
+
 }
